66e40919b7c8a94129d451b03bfa4085d34eb5d4
Ninu <phuong297297@gmail.com>
2023-01-14 18:07:47 +0700

    first comit

---
+++ demogit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demogit
+//
+//  Created by Phung Hieu Lam on 14/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
